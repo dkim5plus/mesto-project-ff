9e2bb918af79406ea66347d451890b07aa926591
KimDa <baronmakaron3000@yandex.ru>
2024-08-04 20:51:01 +0300

    продолжаю пыхтеть

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import { initialCards } from "./cards.js";
 import { openPopup, closePopup } from './scripts/modal.js';
 import { createCard } from './scripts/card.js';
 
+/* Открытия попапа профиля */
+const profileButton = document.querySelector(".profile__edit-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+profileButton.addEventListener('click', function() {
+
+})
+
 /*Редактирование профиля*/
 const formElement = document.forms["edit-profile"];
 const nameForm = formElement.quarySelector('.popup__input_type_name');
@@ -17,3 +26,14 @@ function handleFormSubmit(evt) {
 }
 
 formElement.addEventListener('submit', handleFormSubmit); 
+
+/*Открытие попапа карт*/
+
+
+/* Создание карт */
+const cardContainer = document.querySelector('.places__list');
+
+initialCards.forEach(function(item) {
+    const card = createCard(item, deleteCard);
+    cardContainer.append(card);
+});
--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,18 +1,17 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
-const cardContainer = document.querySelector('.places__list');
-
 // @todo: Функция создания карточки
-export function createCard(item, deleteCard) {
+export function createCard(item, deleteCard, addLike) {
     const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
     const buttonDelete = cardItem.querySelector('.card__delete-button');
     const img = cardItem.querySelector('.card__image');
+    const cardLike = cardElement.querySelector('.card__like-button');
     img.src = item.link;
     img.alt = item.name;
     cardItem.querySelector('.card__title').textContent = item.name;
     buttonDelete.addEventListener('click', deleteCard);
+    cardLike.addEventListener('click', addLike)
     return cardItem;
     
 };
@@ -22,9 +21,7 @@ function deleteCard(evt) {
     evt.target.closest('.card').remove();
 };
 
-// @todo: Вывести карточки на страницу
-initialCards.forEach(function(item) {
-    const card = createCard(item, deleteCard);
-    cardContainer.append(card);
-});
+function addLike(evt) {
+    evt.classList.toggle('card__like-button_is-active')
+};
 
